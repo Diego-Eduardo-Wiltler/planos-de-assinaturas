9e4eb90cedd72ecd23a29075c69c11fb0a9291ac
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-02-01 08:38:29 -0300

    Adicionando comentarios aos produtos

--- app/Http/Controllers/Api/ProdutoController.php
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -21,6 +21,23 @@ class ProdutoController extends Controller
         $this->produtoService = $produtoService;
     }
 
+    /**
+     * GET /api/produtos
+     *
+     * Retorna lista de produtos cadastrados
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: array<ProdutoResource>
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     */
 
     public function getTodosProdutos(): JsonResponse
     {
@@ -35,7 +52,27 @@ class ProdutoController extends Controller
         return $this->errorResponse($result['message']);
     }
 
-
+     /**
+     * GET /api/produtos/{id}
+     *
+     * Retorna um unico produto pelo id
+     *
+     * @urlParam id int required ID do produto a ser encontrado. Example: 1
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: ProdutoResource
+     * }
+     *
+     *  @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     * @param $id ID do produto a ser encontrado
+     *
+     */
     public function getPorIdProdutos($produtoId): JsonResponse
     {
         $result = $this->produtoService->getIdProdutos($produtoId);
@@ -48,7 +85,29 @@ class ProdutoController extends Controller
         return $this->errorResponse($result['message']);
     }
 
-
+    /**
+     * POST /api/produtos
+     *
+     * Cadastra um novo produto
+     *
+     * @bodyParam nome string required Nome do produto. Example: Telefone
+     * @bodyParam descricao string Descricao do produto. Example: Acesse + de 1000 mil ligações a qualquer momento
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: ProdutoResource
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     * @param StoreProdutoFormRequest $request Requisição contendo os dados do produto.
+     * @return JsonResponse
+     *
+     */
     public function store(StoreProdutoFormRequest $request): JsonResponse
     {
         $data = $request->validated();
@@ -63,7 +122,29 @@ class ProdutoController extends Controller
         return $this->errorResponse($result['message']);
     }
 
-
+     /**
+     * POST /api/produtos
+     *
+     * Atualiza um produto existente
+     *
+     * @bodyParam nome string Nome do produtos. Example: Telefone
+     * @bodyParam descricao string Descricao do produtos. Example: Acesse + de 1000 mil ligações a qualquer momento
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: ProdutoResource
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     * @param UpdateProdutoFormRequest $request Requisição contendo os dados do produto.
+     * @return JsonResponse
+     *
+     */
     public function update(UpdateProdutoFormRequest $request, $produtoId): JsonResponse
     {
         $data = $request->validated();
@@ -78,7 +159,27 @@ class ProdutoController extends Controller
         return $this->errorResponse($result['message']);
     }
 
-
+ /**
+     * DELETE api/produtos-deletar/{produtoId}
+     *
+     * Remove um produto existente
+     *
+     * @urlParam id int required ID do produto que irá ser removido. Example: 1
+     *
+     * @response 200 array{
+     *   success: true,
+     *   message: string,
+     *   data: ProdutoResource
+     * }
+     *
+     * @response 400 array{
+     *   success: false,
+     *   message: string
+     * }
+     *
+     * @param int $produtoId ID do produto a ser removido
+     * @return JsonResponse
+     */
     public function destroyProdutos($produtoId)
     {
 
