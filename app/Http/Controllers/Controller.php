278dad778a21d3d07e36f01fc3a053843b36d9a6
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-02-01 07:08:01 -0300

    Adicionando logica para base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+    protected function successResponse(mixed $data, string $message = 'Operação realizada
+    com sucesso', int $code = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function errorResponse(string $message = 'Ocorreu um erro', int $code =
+     400): JsonResponse
+     {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+        ], $code);
+     }
 }
