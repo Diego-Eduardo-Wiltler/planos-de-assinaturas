4205dd2338799934bfebae4c1faf30fc92d8a401
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-02-24 08:48:57 -0300

    Adicionando log e exceptions globais

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,35 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $exception) {
+            Log::error($exception->getMessage(), [
+                'exception' => $exception,
+            ]);
+
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'message' => 'O recurso solicitado não existe ou foi removido.',
+                    'errors' => $exception->getMessage(),
+                    'errorResponse' => 30,
+                ], 200);
+            }
+
+            return response()->json([
+                'message' => 'Something went wrong. Please try again later.'
+            ], 500);
+        });
     })->create();
