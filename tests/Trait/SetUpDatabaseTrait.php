d323296f16465af5fc719b8ca2ed6c084e8ea89e
Diego-Eduardo-Wiltler <diego.eduardo.w@gmail.com>
2025-02-24 09:01:22 -0300

    Adicionando arquivos para testes automatizados

---
+++ tests/Trait/SetUpDatabaseTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Traits;
+
+use App\Models\Produto;
+use App\Models\Plano;
+
+trait SetUpDatabaseTrait
+{
+    protected $planos;
+    protected $produtos;
+
+    protected function setUpDatabase(): void
+    {
+
+    }
+}
